f5d920790f5c30be1bfa19c08c23333132eec58c
Peter Jones <pjones@redhat.com>
2016-02-07 12:51:27 -0500

    Eradicate tooling from shapes; it goes in tracers and rasterizers
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- feedmejack/shapes.py
+++ feedmejack/shapes.py
@@ -3,42 +3,38 @@
 from xyz import *
 
 class Box(object):
-    def __init__(self, xy_min, xy_max, tool=0.0):
+    def __init__(self, xy_min, xy_max):
         self.xy_min = xy_min
         self.xy_max = xy_max
-        self.tool = tool
 
     def __str__(self):
         return "Box(%s,%s)" % (self.xy_min, self.xy_max)
 
     def __contains__(self, point):
-        tw = float(self.tool) / 2.0
-        if float(point.x) + tw < self.xy_min.x:
+        if float(point.x) < self.xy_min.x:
             return False
-        if float(point.x) - tw > self.xy_max.x:
+        if float(point.x) > self.xy_max.x:
             return False
-        if float(point.y) + tw < self.xy_min.y:
+        if float(point.y) < self.xy_min.y:
             return False
-        if float(point.y) - tw > self.xy_max.y:
+        if float(point.y) > self.xy_max.y:
             return False
         return True
 
 class Cube(object):
-    def __init__(self, xyz_min, xyz_max, tool=0.0):
+    def __init__(self, xyz_min, xyz_max):
         if not hasattr(xyz_min, 'z'):
             raise ValueError("xyz_min has no z dimension")
         if not hasattr(xyz_max, 'z'):
             raise ValueError("xyz_max has no z dimension")
         self.xyz_min = xyz_min
         self.xyz_max = xyz_max
-        self.tool = tool
 
     def __str__(self):
         return "Cube(%s,%s)" % (self.xyz_min, self.xyz_max)
 
     def __contains__(self, point):
-        tw = float(self.tool) / 2.0
-        box = Box(self.xyz_min, self.xyz_max, self.tool)
+        box = Box(self.xyz_min, self.xyz_max)
         if not point in box:
             return False
         if not hasattr(point, 'z'):
@@ -51,35 +47,29 @@ class Cube(object):
 
 class Circle(object):
     _strname = "Circle"
-    def __init__(self, center, radius, tool=0.0):
+    def __init__(self, center, radius):
         self.center = center
         self.radius = abs(radius)
-        self.tool = float(tool)
 
     def __str__(self):
         return "%s(%s,%s)" % (self._strname, self.center, self.radius)
 
     def __contains__(self, point):
         d = abs(point.distance(self.center))
-        if self.tool:
-            r = self.radius + ( self.tool / 2 )
-            if d <= r:
-                return True
-        else:
-            r = self.radius
-            if d >= self.radius * 0.99:
-                return True
-            if d <= self.radius * 1.01:
-                return True
+        r = self.radius
+        # la la la why not...
+        if d >= self.radius * 0.95:
+            return True
+        if d <= self.radius * 1.05:
+            return True
         return False
 
 class Plate(Circle):
     _strname = "Plate"
 
     def __contains__(self, point):
-        t2 = self.tool / 2
         d = abs(point.distance(self.center))
-        if d <= self.radius + t2:
+        if d <= self.radius:
             return True
         return False
 
@@ -91,17 +81,12 @@ __all__ = ['Cube', 'Box', 'Circle', 'Plate', 'Ball']
 
 if __name__ == '__main__':
     p = Plate(XY(10,10), 2)
-    print("%s with tool 0" % (p,))
-    for point in [XY(x,y) for y in range(8,13) for x in range(8,13)]:
-        print("%s in %s: %s" % (point, p, point in p))
-    print("%s with tool 0.8" % (p,))
-    p = Plate(XY(10,10), 2, tool=0.8)
+    print("%s" % (p,))
     for point in [XY(x,y) for y in range(8,13) for x in range(8,13)]:
         print("%s in %s: %s" % (point, p, point in p))
 
     b = Ball(XYZ(10,10,10), 2)
-    print("b.center: %s" % (b.center,))
-    print("%s with tool 0" % (b,))
+    print("%s" % (b,))
     raster = []
     for point in [XYZ(x,y,z) for z in range(8,13)
                              for y in range(8,13)
@@ -118,9 +103,8 @@ if __name__ == '__main__':
                     zr.append(p)
         print("%s" % (zr,))
 
-    b = Ball(XYZ(10,10,10), 2, tool=0.8)
-    print("b.center: %s" % (b.center,))
-    print("%s with tool 0.8" % (b,))
+    b = Ball(XYZ(10,10,10), 2)
+    print("%s" % (b,))
     raster = []
     for point in [XYZ(x,y,z) for z in range(8,13)
                              for y in range(8,13)
